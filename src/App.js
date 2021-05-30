a7df314c29ce9373d14e0993b0c92aee389132e2
tiggrra <78557674+tiggrra@users.noreply.github.com>
2021-05-30 12:36:57 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import SearchEngine from "./SearchEngine";
+import Footer from "./Footer";
 import './App.css';
+import "./Footer.css";
 
 function App() {
   return (
     <div className="App">
       <h1>React AJAX Search Engine</h1>
       <SearchEngine />
+      <br />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Footer() {
+    return <div className="Footer">
+    created by <a href="https://github.com/tiggrra/weather-react" target="_blank" rel="noreferrer">tiggrra</a></div>
+}
